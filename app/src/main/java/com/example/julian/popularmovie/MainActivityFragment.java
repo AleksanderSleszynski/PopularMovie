29178a1d2aa51911a6e12c54da77e836e6aa42f6
Rasmuhamed <a.sleszynski@gmail.com>
2015-07-27 18:31:52 +0200

    Add gridView and Image Adapter

--- app/src/main/java/com/example/julian/popularmovie/MainActivityFragment.java
+++ app/src/main/java/com/example/julian/popularmovie/MainActivityFragment.java
@@ -1,11 +1,18 @@
 package com.example.julian.popularmovie;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
 
 
 /**
@@ -20,11 +27,85 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView  = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        final GridView gridView = (GridView) rootView.findViewById(R.id.gridView_movie);
-
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridView_movie);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
 
         return rootView;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if(convertView == null){
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+
+            return imageView;
+        }
+    }
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if(params.length == 0){
+                return null;
+            }
+
+
+            //Declare this outside the try/catch
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //Will contain the raw JSON response as a string
+            String movieJsonStr = null;
+
+            return null;
+        }
     }
 }
