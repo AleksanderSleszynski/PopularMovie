65ee570ac46335bbf068eeba2265a352f94d6811
Rasmuhamed <a.sleszynski@gmail.com>
2015-11-07 22:58:19 +0100

    Implement MovieContract and MovieDbHelper

---
+++ app/src/main/java/com/example/julian/popularmovie/data/MovieContract.java
@@ -0,0 +1,33 @@
+package com.example.julian.popularmovie.data;
+
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+
+/**
+ * Created by julain on 07.11.15.
+ */
+public class MovieContract {
+
+
+    public static long normalizeDate(long startDate) {
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+    public static final class MovieEntry implements BaseColumns{
+
+        public static final String TABEL_NAME = "movie";
+
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_AVERAGE_VOTE = "average_vote";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_FAVOURITE = "favourite";
+        public static final String COLUMN_DATE = "date";
+
+    }
+}
---
+++ app/src/main/java/com/example/julian/popularmovie/data/MovieDbHelper.java
@@ -0,0 +1,37 @@
+package com.example.julian.popularmovie.data;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.julian.popularmovie.data.MovieContract.MovieEntry;
+
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    static final String DATABASE_NAME = "movie.db";
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOVIE_TABEL = "CREATE TABLE " + MovieEntry.TABEL_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MovieEntry.COLUMN_TITLE +  " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_RELEASE_DATE + " INTEGER NOT NULL, " +
+                MovieEntry.COLUMN_DESCRIPTION + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_AVERAGE_VOTE + " INTEGER NOT NULL, " +
+                MovieEntry.COLUMN_POSTER_PATH + " INTEGER NOT NULL, " +
+                MovieEntry.COLUMN_FAVOURITE + " BOOLEAN NOT NULL";
+
+        db.execSQL(SQL_CREATE_MOVIE_TABEL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXIST " + MovieEntry.TABEL_NAME);
+    }
+}
